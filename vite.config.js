dd14a859cd8ba0c4d3c17e85e76537f72c957019
Axel Rea Eklund <axere939@student.liu.se>
2024-12-25 12:47:11 -0700

    Revert "försöker fixa (github pages)"
    This reverts commit ede72dc3e0eb0dd57770c50eb2b12f2c4d54744d.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fad/', // Replace "repository-name" with your GitHub repository name
   plugins: [react()],
 })
